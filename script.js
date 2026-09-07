6683499859ff1f8f2524d55a68b21261278857ec
warisa33 <pakakrong2508@gmail.com>
2026-09-07 11:24:04 +0700

    Initialize script.js for DOM content loaded event

---
+++ script.js
@@ -0,0 +1,4 @@
+document. addEventListener( 'DOMContentLoaded', function () {
+document. getElementByld( 'year') .innerHTML = new Dare() .getFullYear) ;
+console. 1og('Portfolio ของ nantharat พร้อมแล้ว! ') ;
+});
